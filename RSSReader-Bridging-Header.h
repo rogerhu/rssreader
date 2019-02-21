214b0351e40103d96a35d7449946fb46ded94776
Roger Hu <rhu@hearsaycorp.com>
2019-02-21 01:40:17 -0800

    add bridging header

---
+++ RSSReader-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  RSSReader-Bridging-Header.h
+//  RSSReaderSwift
+//
+//  Created by Roger Hu on 2/20/19.
+//  Copyright © 2019 Good. All rights reserved.
+//
+
+#ifndef RSSReader_Bridging_Header_h
+#define RSSReader_Bridging_Header_h
+
+#import <GD/GDUtility.h>
+
+#endif /* RSSReader_Bridging_Header_h */
